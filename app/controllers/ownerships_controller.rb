50ec0c6b780f1c86c7fec5acc5852499de68c188
miyatani <ec2-user@ip-172-31-5-122.us-east-2.compute.internal>
2018-03-03 09:05:25 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -14,19 +14,24 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
+    else
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
     end
 
     redirect_back(fallback_location: root_path)
   end
-
+  
   def destroy
     @item = Item.find(params[:item_id])
 
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
-    end
-
-    redirect_back(fallback_location: root_path)
+    else
+    current_user.unhave(@item)
+    flash[:success] = '商品の Have を解除しました。'
   end
+    redirect_back(fallback_location: root_path)
+end
 end
